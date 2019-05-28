6cb9ca7ce6f06c7d215856fd615469e9070f5f2d
Richard Lee <richlee81192@gmail.com>
2019-05-28 18:00:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-var superheroines = ["catwoman", "she-hulk", "mystique"]
-
-superheroines.push("wonder woman")
-
-var cities = ["New York", "San Francisco"]
-
-cities.shift("Philidelphia")
-
 function addElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
