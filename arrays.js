4cebf71a2fed8d0ae802f5bafa2abff8583aea77
Richard Lee <richlee81192@gmail.com>
2019-05-28 18:04:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,13 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-  return array
+  arrayNew = [element, ...array]
+  return arrayNew
 }
 
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  arrayNew = [element, ...array]
-  return arrayNew
+  array.unshift(element)
+  return array
+  
 }
