27901b12ac7573869e1495cfc55fa3a90f45f228
Richard Lee <richlee81192@gmail.com>
2019-05-28 17:59:41 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+var superheroines = ["catwoman", "she-hulk", "mystique"]
+
+superheroines.push("wonder woman")
+
+var cities = ["New York", "San Francisco"]
+
+cities.shift("Philidelphia")
+
+function addElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+  return array
+}
